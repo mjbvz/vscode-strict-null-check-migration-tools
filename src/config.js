4f203325b9590c5eec0aab30a14370ea2c4fbbb6
Matt Bierner <matb@microsoft.com>
2018-10-22 17:40:24 -0700

    Exclude other importer of windows process tree
    Still looking into proper fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports.targetTsconfig = 'tsconfig.strictNullChecks.json';
 
 module.exports.skippedFiles = new Set([
-    'vs/workbench/parts/terminal/node/windowsShellHelper.ts'
+    'vs/workbench/parts/terminal/node/windowsShellHelper.ts',
+    'vs/base/node/ps.ts',
+    'vs/code/electron-browser/processExplorer/processExplorerMain.ts'
 ])
