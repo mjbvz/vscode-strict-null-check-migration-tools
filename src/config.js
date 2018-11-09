c70fa91f18d65460969bc83a627d6cda41d1d7d0
Matt Bierner <matb@microsoft.com>
2018-11-08 17:34:38 -0800

    Remove excluded files
    These files should be able to be safely included now

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 module.exports.targetTsconfig = 'tsconfig.strictNullChecks.json';
 
 module.exports.skippedFiles = new Set([
-    'vs/workbench/parts/terminal/node/windowsShellHelper.ts',
-    'vs/base/node/ps.ts',
-    'vs/code/electron-browser/processExplorer/processExplorerMain.ts'
+    
 ])
